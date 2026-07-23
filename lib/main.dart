258c51c9e2fd67c1ed6eb6e3c2be164763d36597
Sumit <shresthaswen80@gmail.com>
2026-07-23 15:42:14 +0545

    fixing codes changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,13 @@ import 'package:devansh/Router/router.dart';
 import 'package:devansh/firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'package:flutter/material.dart';
 
 import 'package:flutter/services.dart';
 
 Future<void> main() async {
- 
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setSystemUIOverlayStyle(
@@ -20,14 +19,13 @@ Future<void> main() async {
     ),
   );
 
- 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  
+
   FirebaseFirestore.instance.settings = const Settings(
     persistenceEnabled: true,
     webExperimentalForceLongPolling: true,
   );
-  
+  usePathUrlStrategy();
   runApp(const MyApp());
 }
 
@@ -36,7 +34,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return MaterialApp.router(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
